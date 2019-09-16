4ab872b17a292c4943ff033ca12e4dd3da60d136
Eskild Skov Pedersen <eskild.skov.pedersen@gmail.com>
2019-09-16 07:54:30 +0200

    Added arrow to medications

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, render_template, Response
 from fpdf import FPDF
 import textwrap
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route("/", methods=('GET', 'POST'))
+@app.route("/", methods=('GET', 'POST'))
 def index():
     if request.method == 'GET':
         return render_template('index.html')
@@ -20,26 +20,24 @@ def index():
         response.headers['Content-Disposition'] = 'inline; filename=%s.pdf' % 'Kurve'
         return response
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
-
-
 def create_pdf(faste_medisiner, behovsmedisiner) :
     pdf = FPDF('P', 'mm', 'A4')
+    pdf.add_font('DejaVu', '', 'DejaVuSans.ttf', uni=True)
+    pdf.set_font('DejaVu', '', 14)
 
     while faste_medisiner or behovsmedisiner:
         pdf.add_page()
-        pdf.set_font('Arial', '', 10)
+
         pos = 106.5 # Start faste medisiner
         pdf.set_xy(11, pos)
 
         i = 1
         while faste_medisiner and i <= 15 :
             medisin = faste_medisiner.pop(0)
+            medisin = '→ ' + medisin
 
             if len(medisin) > 30 :
-                pdf.set_font('Arial', '', 8) # go small
+                pdf.set_font_size(8) # go small
 
                 if len(medisin) > 40 : # go multi line!
                     medisin = textwrap.fill(medisin, 40)
@@ -58,8 +56,7 @@ def create_pdf(faste_medisiner, behovsmedisiner) :
                     pdf.cell(10, 10, medisin, 0)
                     pos = pos + 8.9
 
-                pdf.set_font('Arial', '', 10)
-                # pdf.set_xy(11, orig_pos)
+                pdf.set_font_size(10)
 
             else :
                 pdf.cell(40, 10, medisin, 0)
@@ -71,13 +68,15 @@ def create_pdf(faste_medisiner, behovsmedisiner) :
         pdf.add_page()
         pdf.set_auto_page_break(False)
 
-        pdf.set_font('Arial', '', 10)
+        pdf.set_font_size(10)
         pos = 233 # Start behovsmedisiner
         pdf.set_xy(4, pos)
 
         i = 1
         while behovsmedisiner and i <= 10 :
             medisin = behovsmedisiner.pop(0)
+            medisin = '→ ' + medisin
+
             pdf.cell(40, 10, medisin, 0)
             pos = pos + 5.5
             pdf.set_xy(4, pos)
@@ -85,3 +84,6 @@ def create_pdf(faste_medisiner, behovsmedisiner) :
             i += 1
 
     return pdf.output(name = 'kurve', dest = 'S').encode('latin-1')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
